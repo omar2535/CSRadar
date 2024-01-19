e575593ef50d8e1e67a7cdcb823de77fe9c7c78c
Omar <omar2535@alumni.ubc.ca>
2024-01-18 22:38:04 -0800

    add better prints

--- src/main.rs
+++ src/main.rs
@@ -103,7 +103,8 @@ fn main() {
 
 
         // clear the screen
-        clearscreen::clear().expect("Failed to clear screen!");
+        // clearscreen::clear().expect("Failed to clear screen!");
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
         thread::sleep(SLEEP_TIME);
     }
 
