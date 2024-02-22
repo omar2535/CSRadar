a2b594b3cf5e1baa8ae60674f0a1f7789e0afb5a
Omar2535 <omar2535@alumni.ubc.ca>
2024-02-22 00:42:20 -0500

    Update offsets

--- src/cs2_offsets/server_dll.rs
+++ src/cs2_offsets/server_dll.rs
@@ -1,6 +1,6 @@
 /*
  * All offsets dumped with: https://frezzyhook.com
- * Thu, 15 Feb 2024 02:25:14 +0000
+ * Sat, 17 Feb 2024 07:52:05 +0000
  */
 
 #![allow(non_snake_case, non_upper_case_globals)]
