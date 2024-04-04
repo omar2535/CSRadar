4c2a69becca2c7af1724495daee1770035167b44
Omar2535 <omar2535@alumni.ubc.ca>
2024-04-03 23:43:01 -0400

    Fix offsets

--- src/cs2_offsets/offsets.rs
+++ src/cs2_offsets/offsets.rs
@@ -8,14 +8,14 @@ pub mod cs2_dumper {
         // Module: client.dll
         pub mod client_dll {
             pub const dwCSGOInput: usize = 0x192B620;
-            pub const dwEntityList: usize = 0x18C2D58;
+            pub const dwEntityList: usize = 0x18C1DB8;
             pub const dwGameEntitySystem: usize = 0x19E0790;
             pub const dwGameEntitySystem_getHighestEntityIndex: usize = 0x1510;
             pub const dwGameRules: usize = 0x191FCA0;
             pub const dwGlobalVars: usize = 0x172ABA0;
             pub const dwGlowManager: usize = 0x19200C0;
-            pub const dwLocalPlayerController: usize = 0x1912578;
-            pub const dwLocalPlayerPawn: usize = 0x17371A8;
+            pub const dwLocalPlayerController: usize = 0x1911578;
+            pub const dwLocalPlayerPawn: usize = 0x17361E8;
             pub const dwPlantedC4: usize = 0x1928AD8;
             pub const dwPrediction: usize = 0x1737070;
             pub const dwSensitivity: usize = 0x19209E8;
